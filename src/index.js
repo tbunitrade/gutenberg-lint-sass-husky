5689a9791a1497ae8784dcd246fd5d91df4389da
osony <osony@softserveinc.com>
2023-03-15 19:06:46 +0200

    init updates

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import { registerBlockType } from '@wordpress/blocks';
+import './style.scss';
+import Edit from './edit';
+import save from './save';
+import metadata from './block.json';
+
+const x = 0;
+
+registerBlockType( metadata.name, {
+	/**
+	 * @see ./edit.js
+	 */
+	edit: Edit,
+
+	/**
+	 * @see ./save.js
+	 */
+	save,
+} );
